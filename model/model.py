4e74743cfc2327b0905323fbce67685da70f50c0
ducky-hong <pacifings@gmail.com>
2018-12-11 23:29:14 -0500

    fix typo

--- model/model.py
+++ model/model.py
@@ -77,7 +77,7 @@ class MobileNetV2(BaseModel):
         # building first layer
         assert input_size % 32 == 0
         input_channel = int(input_channel * width_mult)
-        self.last_channel = ipython -m caffe2.python.models.download -i squeezenetnt(last_channel * width_mult) if width_mult > 1.0 else last_channel
+        self.last_channel = int(last_channel * width_mult) if width_mult > 1.0 else last_channel
         self.features = [conv_bn(3, input_channel, 2)]
         # building inverted residual blocks
         for t, c, n, s in interverted_residual_setting:
